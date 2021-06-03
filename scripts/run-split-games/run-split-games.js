792f5465d6d70df2af69c235b98ade9a0eecb3dc
Adam Morris <aimorris@aimorris.com>
2021-06-03 10:21:57 +1000

    one script for everything!

--- scripts/run-split-games/run-split-games.js
+++ scripts/run-split-games/run-split-games.js
@@ -9,13 +9,9 @@ const incompatible2021 = ['05', '06', '07', '08', '09', '10', '11', '12'];
 years.forEach(year => {
   months.forEach(month => {
     if (!(year == '2014' && incompatible2014.includes(month)) && !(year == '2021' && incompatible2021.includes(month))) {
-      execSync(`wget https://cubox.dev/files/chess/black-${year}-${month}.pgn.xz`);
-      execSync(`xz -d black-${year}-${month}.pgn.xz`);
-      execSync(`node ../split-games/split-games.js blackkbn-${year}-${month}.pgn black`);
+      execSync(`wget https://cubox.dev/files/chess/black-${year}-${month}.pgn.xz; xz -d black-${year}-${month}.pgn.xz; node ../split-games/split-games.js blackkbn-${year}-${month}.pgn black; rm black-${year}-${month}.pgn`);
 
-      execSync(`wget https://cubox.dev/files/chess/white-${year}-${month}.pgn.xz`);
-      execSync(`xz -d white-${year}-${month}.pgn.xz`);
-      execSync(`node ../split-games/split-games.js whitekbn-${year}-${month}.pgn white`);
+      execSync(`wget https://cubox.dev/files/chess/white-${year}-${month}.pgn.xz; xz -d white-${year}-${month}.pgn.xz; node ../split-games/split-games.js whitekbn-${year}-${month}.pgn white; rm white-${year}-${month}.pgn`);
     }
   })
 });
