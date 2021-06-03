91f4ce7e1ddd18fe65288afb839679acf5f3d1f0
Adam Morris <aimorris@aimorris.com>
2021-06-03 10:17:34 +1000

    remove pgn

--- scripts/run-split-games/run-split-games.js
+++ scripts/run-split-games/run-split-games.js
@@ -9,7 +9,12 @@ const incompatible2021 = ['05', '06', '07', '08', '09', '10', '11', '12'];
 years.forEach(year => {
   months.forEach(month => {
     if (!(year == '2014' && incompatible2014.includes(month)) && !(year == '2021' && incompatible2021.includes(month))) {
+      execSync(`wget https://cubox.dev/files/chess/black-${year}-${month}.pgn.xz`);
+      execSync(`xz -d black-${year}-${month}.pgn.xz`);
       execSync(`node ../split-games/split-games.js blackkbn-${year}-${month}.pgn black`);
+
+      execSync(`wget https://cubox.dev/files/chess/white-${year}-${month}.pgn.xz`);
+      execSync(`xz -d white-${year}-${month}.pgn.xz`);
       execSync(`node ../split-games/split-games.js whitekbn-${year}-${month}.pgn white`);
     }
   })
