e621b69a646d4a215b17d42b792decd477610727
Adam Morris <aimorris@aimorris.com>
2021-06-04 12:30:14 +1000

    modify run script

--- scripts/run-split-games/run-split-games.js
+++ scripts/run-split-games/run-split-games.js
@@ -1,17 +1,19 @@
 const { execSync } = require("child_process");
 
-const years = ['2014', '2015', '2016', '2017', '2018', '2019', '2020'];
+const years = ['2014', '2015', '2016', '2017', '2018', '2019'];
 const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
 
 const incompatible2014 = ['01', '02', '03', '04', '05', '06'];
-const incompatible2021 = ['05', '06', '07', '08', '09', '10', '11', '12'];
+const incompatible2019 = ['09', '10', '11', '12'];
 
 years.forEach(year => {
   months.forEach(month => {
-    if (!(year == '2014' && incompatible2014.includes(month)) && !(year == '2021' && incompatible2021.includes(month))) {
-      execSync(`wget https://cubox.dev/files/chess/black-${year}-${month}.pgn.xz; xz -d black-${year}-${month}.pgn.xz; node ../split-games/split-games.js blackkbn-${year}-${month}.pgn black; rm black-${year}-${month}.pgn`);
+    if (!(year == '2014' && incompatible2014.includes(month)) && !(year == '2019' && incompatible2019.includes(month))) {
+      execSync(`xz -dk /www/cubox/files/chess/blackkbn-${year}-${month}.pgn.xz`);
+      execSync(`node ../scripts/split-games/split-games.js /www/cubox/files/chess/blackkbn-${year}-${month}.pgn black`);
 
-      execSync(`wget https://cubox.dev/files/chess/white-${year}-${month}.pgn.xz; xz -d white-${year}-${month}.pgn.xz; node ../split-games/split-games.js whitekbn-${year}-${month}.pgn white; rm white-${year}-${month}.pgn`);
+      execSync(`xz -dk /www/cubox/files/chess/whitekbn-${year}-${month}.pgn.xz`);
+      execSync(`node ../scripts/split-games/split-games.js /www/cubox/files/chess/whitekbn-${year}-${month}.pgn white`);
     }
   })
 });
