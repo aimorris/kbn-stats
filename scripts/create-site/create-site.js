2987f75d3610441ef690cf972dbd5dbf344715e1
Adam Morris <aimorris@aimorris.com>
2021-06-09 18:40:38 +1000

    monthly data

--- scripts/create-site/create-site.js
+++ scripts/create-site/create-site.js
@@ -116,17 +116,17 @@
 
 const fs = require('fs');
 
-const years = ['2014', '2015', '2016', '2017', '2018', '2019'];
+const years = ['2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'];
 const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
 
 const incompatible2014 = ['01', '02', '03', '04', '05', '06'];
-const incompatible2019 = ['09', '10', '11', '12'];
+const incompatible2021 = ['06', '07', '08', '09', '10', '11', '12'];
 
 const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 years.forEach(year => {
   months.forEach(month => {
-    if (!(year == '2014' && incompatible2014.includes(month)) && !(year == '2019' && incompatible2019.includes(month))) {
+    if (!(year == '2014' && incompatible2014.includes(month)) && !(year == '2021' && incompatible2021.includes(month))) {
       fs.writeFile(`./site/${year}-${month}.html`, `<!DOCTYPE html>
       <html>
       
